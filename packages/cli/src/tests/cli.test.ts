4b7a34164a4b5d5234fe9e7b8cfe613f32f3860a
Elizabeth Craig <elcraig@microsoft.com>
2022-05-17 23:59:25 -0700

    fix small test issue

--- packages/cli/src/tests/cli.test.ts
+++ packages/cli/src/tests/cli.test.ts
@@ -2,22 +2,20 @@ import * as assert from "assert";
 import baretest from "baretest";
 import { inTmpDir } from "@boll/test-internal";
 import { Cli } from "../cli";
-import { exists } from "fs";
+import { existsSync } from "fs";
 import { NullLogger } from "@boll/core";
-import { promisify } from "util";
-const existsAsync = promisify(exists);
 
 export const test: any = baretest("CLI");
 
 test("should create example config file when invoked with `init`", async () => {
   await inTmpDir(async () => {
-    const configExistsPrecondition = await existsAsync("boll.config.js");
+    const configExistsPrecondition = existsSync("boll.config.js");
     assert.strictEqual(false, configExistsPrecondition);
 
     const sut = new Cli(NullLogger);
     await sut.run(["init"]);
 
-    const configExistsExpected = await existsAsync(".boll.config.js");
+    const configExistsExpected = existsSync(".boll.config.js");
     assert.strictEqual(true, configExistsExpected);
   });
 });
